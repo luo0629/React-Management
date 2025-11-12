3eea821520ef1ca2ee68b00405c77b46a374aa10
neflibata <859332452@qq.com>
2025-11-12 17:35:23 +0800

    点击menu添加tag，tag跳转页面，删除逻辑实现

--- src/components/commonTag.tsx
+++ src/components/commonTag.tsx
@@ -1,24 +1,85 @@
 import React from "react";
 import {Space, Tag} from "antd";
 //获取store中的数据 就需要使用useSelector
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { closeTag,setCurrentTag } from "../store/reducers/tab";
+//获得当前路由的信息 是由Router中的useLocation的钩子函数
+import { useLocation,useNavigate } from "react-router";
+
+
+
+interface TabListType{
+    path:string,
+    name?:string,
+    label:string
+}
 
 const CommonTag=()=>{
     //使用store来储存状态信息，并使用useSelector来获取状态信息
     //获取store里面的状态信息
     const tabList=useSelector((state:any)=>state.tab.tabList);
+    //获取当前选中的数据
+    const currentMenu=useSelector((state:any)=>state.tab.currentMenu);
+    const dispatch=useDispatch();
+    const action=useLocation();
+    const navigate=useNavigate();
+
     console.log(tabList);
-    const handleClose=()=>{
+
+    const handleClose=(item:TabListType,index:number)=>{
+        let length=tabList.length-1;
+        dispatch(closeTag(item));
+        //如果关闭不是当前的tag
+        if(item.path !== action.pathname){
+            return
+        }
+        //如果关闭是当前的tag
+        if(index===length){
+            //最后一个tag关闭时 要向前移动
+            //设置当前数据
+            const currentData=tabList[index-1];
+            dispatch(setCurrentTag(currentData));
+            //实现路由跳转
+            navigate(currentData.path);
+        }else{
+            //不是最后一个tag关闭时 要向后移动
+            //如果tag至少存在一个数据，则选中后一个tag
+            if (tabList.length>1){
+                const nextData=tabList[index-1];
+                dispatch(setCurrentTag(nextData));
+                //实现路由跳转
+                navigate(nextData.path);
+            }
+
+        }
 
     }
+    //点击tag事件
+    const handelChange=(tag:TabListType)=>{
+        dispatch(setCurrentTag(tag));
+        navigate(tag.path);
+
+    }
+    //多级html渲染 建议拆分成多个函数进行
+    const setTag=(falg:boolean,item:TabListType,index:number)=>{
+        return (
+            falg?<Tag key={item.name} color="#55acee" closeIcon onClose={()=>handleClose(item,index)}>{item.label}</Tag>
+            :<Tag onClick={()=>handelChange(item)} key={item.name}>{item.label}</Tag>
+        )
+
+    }
+
 
     //Tag的选中颜色可以根据路由来聚焦变化 只有当前路由对应的Tag显示颜色 其余的Tag都显示默认颜色即可
     return(
         <Space size={[0,8]} wrap className="pt-[24px] pl-[20px]">
-            <Tag>首页</Tag>
+            {/* <Tag>首页</Tag>
             <Tag color="#55acee" closeIcon onClose={()=>handleClose()}>
                 用户列表
-            </Tag>
+            </Tag> */}
+            {
+                currentMenu.name&&tabList.map((item:TabListType,index:number)=>(setTag(item.path===currentMenu.path,item,index)))
+            }
         </Space>
     );
 
--- src/store/reducers/tab.ts
+++ src/store/reducers/tab.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, current } from '@reduxjs/toolkit';
 
 // 创建 tab 状态切片，用于管理侧边栏和标签页相关状态
 const tabSlice = createSlice({
@@ -6,13 +6,16 @@ const tabSlice = createSlice({
     // 初始状态
     initialState: {
         isCollapse: false, // 侧边栏是否折叠
+        //点击菜单项的数据记录
         tabList:[
             {
                 path:'/',
                 name:'home',
                 label:'首页', //代表Tag的名字
             }
-        ]
+        ],
+        //选中数据的存储
+        currentMenu:{}
     },
     // 定义 reducers
     reducers: {
@@ -21,20 +24,41 @@ const tabSlice = createSlice({
             state.isCollapse = !state.isCollapse;
         },
         selectMenuList:(state,{payload:val})=>{
-            //首页是不需要设置的
-            if(val.name!=='name'){
+            //首页是不需要设置的 不是首页时
+            if(val.name!=='home'){
+                state.currentMenu=val
                 //如果已经存在的话就不需要添加了 所以这里要进行一个判断
-                const reductIndex=state.tabList.findIndex(item=>{item.name==val.name});
+                const reductIndex=state.tabList.findIndex(item=> item.name === val.name);
+
+                console.log(val,'val');
+                console.log(reductIndex,'reductIndex');
                 //只有不存在的时候才会添加
-                if (reductIndex===-1){
+                if (reductIndex === -1){
                     state.tabList.push(val);
                 }
+                //如果当前是首页并且tabList内容中只有一个
+            }else if (val.name==='home' &&state.tabList.length===1){
+                //是首页时
+                state.currentMenu={}
+            }
+        },
+        closeTag:(state,{payload:val})=>{
+            let res=state.tabList.findIndex(item=>item.name===val.name);
+            state.tabList.splice(res,1);
+        },
+        //设置当前选中的tag数据
+        setCurrentTag:(state,{payload:val})=>{
+            if(val.name==='home'){
+                state.currentMenu={}
+            }else{
+                state.currentMenu=val
             }
         }
+
     }
 });
 
 // 导出 action creators
-export const { collapseMenu,selectMenuList } = tabSlice.actions;
+export const { collapseMenu,selectMenuList,closeTag,setCurrentTag } = tabSlice.actions;
 // 导出 reducer
 export default tabSlice.reducer;
